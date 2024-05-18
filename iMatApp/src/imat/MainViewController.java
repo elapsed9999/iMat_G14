e0efddbbc922db4a295b5298dadc21053b94f290
Adam Kinman <04adam.kinman@gmail.com>
2024-05-18 14:09:57 +0200

    Gjorde product cards lite snyggare

--- iMatApp/src/imat/MainViewController.java
+++ iMatApp/src/imat/MainViewController.java
@@ -8,7 +8,6 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
